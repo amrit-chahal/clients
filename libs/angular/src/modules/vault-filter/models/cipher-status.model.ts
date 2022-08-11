a6ba417733308db9ce6d9e34260575809c5d58a1
Jacob Fink <jfink@bitwarden.com>
2022-08-11 12:45:54 -0400

    [EC-14] begin refactoring vault filter with more abstraction

--- libs/angular/src/modules/vault-filter/models/cipher-status.model.ts
+++ libs/angular/src/modules/vault-filter/models/cipher-status.model.ts
@@ -1,1 +1,5 @@
-export type CipherStatus = "all" | "favorites" | "trash";
+import { CipherType } from "@bitwarden/common/enums/cipherType";
+import { ITreeNodeObject } from "@bitwarden/common/models/domain/treeNode";
+
+export type CipherFilter = ITreeNodeObject & CipherStatus;
+export type CipherStatus = "all" | "favorites" | "trash" | CipherType;
